71544eb23b340e3bacfe90561a17186710a747c9
Vlad Meshcherinov <vladislav1310@gmail.com>
2023-04-01 23:59:12 +0400

    feat: implement authorization, add protected route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,36 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import MainScreen from '../Test';
+import LoginScreen from '../LoginScreen/LoginScreen';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const authorized = localStorage.getItem('authorized');
+
+  const handleLogin = () => {
+    localStorage.setItem('authorized', true);
+    setIsLoggedIn(true)
+  }
+
   return (
     <div className="App">
       <div className="page">
         <Routes>
           <Route path='/'
-            element={<MainScreen />}
+            element={
+              <ProtectedRoute isLoggedIn={authorized}>
+                <MainScreen />
+              </ProtectedRoute>
+            }
           />
+          <Route path='/sign-in' element={
+            !isLoggedIn ?
+              <LoginScreen handleLogin={handleLogin} /> : <Navigate to='/' />
+          } />
         </Routes>
       </div>
     </div>
--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -1,9 +1,7 @@
-import './Login.css';
-import { Link } from 'react-router-dom';
 import React from 'react';
 import { useFormWithValidation } from '../../hooks/useFormWithValidation';
 
-export default function Login(props) {
+export default function LoginScreen(props) {
   const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation();
 
   const handleFormSubmit = (evt) => {
@@ -16,12 +14,9 @@ export default function Login(props) {
   }, [resetForm]);
 
   return (
-    <main className="signin-signup">
-      <div className="form form_login">
-        <Link to="/">
-          <div className="header__logo"></div>
-        </Link>
-        <h1 className="form__title">Рады видеть!</h1>
+    <main className="signin">
+      <div className="form form_signin">
+        <h1 className="form__title">Simple Hotel Check</h1>
         <form className="form__content" onSubmit={handleFormSubmit}>
           <label className="form__label" htmlFor="email">E-mail</label>
           <input
@@ -47,12 +42,6 @@ export default function Login(props) {
           <span className="input__error-text">{errors.password || ''}</span>
           <button type="submit" aria-label="Войти" className={`button form__submit-button ${!isValid && 'form__submit-button_disabled'}`} disabled={!isValid}>Войти</button>
         </form>
-        <div className="signin-signup__link-wrap">
-          <p className="signin-signup__text">Ещё не зарегистрированы?</p>
-          <Link to="/sign-up" style={{ textDecoration: 'none' }}>
-            <p className="link signin-signup__link">Регистрация</p>
-          </Link>
-        </div>
       </div>
     </main>
   );
